f65e67da1c970ffd1587049e9d06cb75bfb87969
ashray <ashray@finzy.com>
2020-03-28 14:37:36 +0530

    Added test script in package.json.
    * Added nyc for coverage.
    * Added ts-lint for linting.

--- test/bootstrap/server-bootstrapper.spec.ts
+++ test/bootstrap/server-bootstrapper.spec.ts
@@ -3,10 +3,15 @@ import {AddressInfo} from 'net';
 import {describe, it} from 'mocha';
 import {ServerBootstrapper} from '../../src/bootstrap/server-bootstrapper';
 
-describe('should bootstrap server', function () {
+describe('should bootstrap server', async function () {
+    const server = await new ServerBootstrapper().bootstrap();
+
     it('should start server at port 15000', async function () {
-        const server = await new ServerBootstrapper().bootstrap();
         const address = server.address() as AddressInfo;
         expect(address.port).to.equal(15000);
+    });
+
+    after(function() {
+        server.close();
     });
 });
