6b6cc7a8a2cb1befa36e048578bc8f5a22cee0b4
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-03-28 18:13:09 +0530

    Bootstrapping database in index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import {ApplicationServer} from './bootstrap/application-server';
+import {DatabaseBootstrapper} from './bootstrap/database-bootstrapper';
 
 async function bootUp() {
-    const application = new ApplicationServer().initialize();
+    const applicationServer = new ApplicationServer();
+    const application = applicationServer.initialize();
     application.listen(15000);
+
+    await applicationServer.container.get(DatabaseBootstrapper).bootstrap();
 }
 
 bootUp();
