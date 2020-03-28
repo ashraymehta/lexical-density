8d61032d64910d1ccb5a2f82628d262715d14bde
ashray <ashray@finzy.com>
2020-03-28 15:22:22 +0530

    Calculating per-sentence lexical-density.

--- test/services/lexical-density-calculator.spec.ts
+++ test/services/lexical-density-calculator.spec.ts
@@ -10,8 +10,19 @@ describe('should calculate lexical density', function () {
     it('should calculate lexical density for a single sentence', async function () {
         when(nonLexicalWordsService.findAllNonLexicalWords()).thenResolve(['a', 'to', 'the', 'have']);
 
-        const lexicalDensity = await lexicalDensityCalculator.calculateForSentence('Kim loves going to the cinema');
+        const lexicalDensity = await lexicalDensityCalculator.calculate('Kim loves going to the cinema');
 
-        expect(lexicalDensity).to.equal(0.67);
+        expect(lexicalDensity.overallLexicalDensity).to.equal(0.67);
+        expect(lexicalDensity.sentenceLexicalDensities).to.deep.equal([0.67]);
+    });
+
+    it('should calculate lexical density for multiple sentences', async function () {
+        when(nonLexicalWordsService.findAllNonLexicalWords()).thenResolve(['a', 'to', 'the', 'have']);
+
+        const sentences = `Kim loves going to the cinema. John also likes to visit the cinema.`;
+        const result = await lexicalDensityCalculator.calculate(sentences);
+
+        expect(result.overallLexicalDensity).to.equal(0.69);
+        expect(result.sentenceLexicalDensities).to.deep.equal([0.67, 0.71]);
     });
 });
